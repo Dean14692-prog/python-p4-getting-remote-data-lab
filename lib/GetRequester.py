ccdf3ddb1089993b5dfa3e5ab35e6b1213a2fedd
Dean14692-prog <dennismuasyangui@gmail.com>
2025-06-18 13:58:45 +0300

    commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,13 @@
 import requests
-import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content  # returns raw byte content
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return response.json()   # converts JSON response to Python list/dict
